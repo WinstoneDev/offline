c78f28c9b64f51465394868fa49088ab25879ce6
Winstone <70216345+WinstoneDev@users.noreply.github.com>
2022-05-09 01:25:43 +0200

    Config

--- shared/config.lua
+++ shared/config.lua
@@ -35,15 +35,20 @@ Config.Items = {
     ['weapon_pistol'] = {label = "Beretta", weight = 1, props = "w_pi_pistol"},
     ['weapon_combatpistol'] = {label = "Glock-17", weight = 1, props = "w_pi_combatpistol"},
     ['idcard'] = {label = "Carte d'identité", weight = 0.005, props = "ch_prop_swipe_card_01c"},
+    ['carte'] = {label = "Carte banquaire", weight = 0.005, props = "ch_prop_swipe_card_01c"},
+
     ['tshirt'] = {label = "Haut", weight = 0.005, props = "prop_ld_tshirt_01"},
     ['pants'] = {label = "Pantalon", weight = 0.005, props = "prop_cs_box_clothes"},
     ['shoes'] = {label = "Chaussure", weight = 0.005, props = "prop_ld_shoe_01"},
     ['helmet'] = {label = "Chapeau", weight = 0.005, props = "prop_cs_box_clothes"},
     ['glasses'] = {label = "Lunette", weight = 0.005, props = "prop_cs_sol_glasses"},
     ['chain'] = {label = "Chaine", weight = 0.005, props = "prop_cs_box_clothes"},
+    ['bags'] = {label = "Sacs", weight = 0.005, props = "prop_cs_box_clothes"},
 }
 
 Config.InsertItems = {
+    ['idcard'] = true,
+    ['carte'] = true,
     ['phone'] = true,
     ['weapon_pistol'] = true,
     ['weapon_combatpistol'] = true,
@@ -54,6 +59,7 @@ Config.InsertItems = {
     ['helmet'] = true,
     ['glasses'] = true,
     ['chain'] = true,
+    ['bags'] = true,
 }
 
 Config.ResourcesClientEvent = {
@@ -67,4 +73,50 @@ Config.PickupModelCollision = {
 	["p_ld_stinger_s"] = true,
 	["prop_barrier_work05"] = true,
 	["prop_mp_cone_02"] = true
+}
+
+Config.zoneClothShop = {
+    Binco = {
+        {coords = vector3(-822.42, -1073.55, 10.33)},
+        {coords = vector3(75.34, -1393.00, 28.38)},
+        {coords = vector3(425.59, -806.15, 28.49)},
+        {coords = vector3(4.87, 6512.46, 30.88)},
+        {coords = vector3(1693.92, 4822.82, 41.06)},
+        {coords = vector3(1196.61, 2710.25, 37.22)},
+        {coords = vector3(-1101.48, 2710.57, 18.11)},
+        Header = "shopui_title_lowendfashion2",
+        BlipId = 73,
+        BlipColor = 5,
+        BlipScale = 0.7,
+        Type = "Cloth"
+    },
+    Suburban = {
+        {coords = vector3(-1193.16, -767.98, 16.32)},
+        {coords = vector3(125.77, -223.9, 53.56)},
+        {coords = vector3(614.19, 2762.79, 41.09)},
+        {coords = vector3(-3170.54, 1043.68, 19.86)},
+        Header = "shopui_title_midfashion",
+        BlipId = 73,
+        BlipColor = 5,
+        BlipScale = 0.7,
+        Type = "Cloth",
+    },
+    Ponsonbys = {
+        {coords = vector3(-709.86, -153.1, 36.42)},
+        {coords = vector3(-163.37, -302.73, 38.73)},
+        {coords = vector3(-1450.42, -237.66, 48.81)},
+        Header = "shopui_title_highendfashion",
+        BlipId = 73,
+        BlipColor = 5,
+        BlipScale = 0.7,
+        Type = "Cloth"
+    },
+    Masks = {
+        {coords = vector3(-1337.25, -1277.54, 3.88)},
+        Header = "shopui_title_movie_masks",
+        BlipId = 362,
+        BlipColor = 5,
+        BlipScale = 0.7,
+        Type = "Mask"
+    }
 }
---
+++ shared/sv_config.lua
@@ -0,0 +1,40 @@
+Shared = {}
+
+Shared.Anticheat = {
+    BlacklistVehicle = true,
+    WhitelistObject = true,
+    BlacklistPed = true,
+
+    ListVehicles = {
+        [GetHashKey('tug')] = true,
+        [GetHashKey('jet')] = true,
+        [GetHashKey('oppressor')] = true,
+        [GetHashKey('oppressor2')] = true,
+        [GetHashKey('rhino')] = true
+    },
+
+    ListPeds = {
+        [""] = true,
+    },
+
+    ListObjects = {
+        [""] = true,
+    },
+
+    ListExplosions = {
+        [1] = true, 
+        [2] = true, 
+        [4] = true, 
+        [5] = true, 
+        [25] = true, 
+        [32] = true, 
+        [33] = true, 
+        [35] = true, 
+        [36] = true, 
+        [37] = true, 
+        [38] = true,
+
+        [7] = true,
+        [9] = true,
+    }
+}

06b8e81c4ec95edab47351c6e2142dc767d011cb
Winstone <70216345+WinstoneDev@users.noreply.github.com>
2022-05-09 01:25:18 +0200

    Create blips.lua

---
+++ module/utils/blips.lua
@@ -0,0 +1,59 @@
+local blips = {
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-1222.26, -906.86, 11.40)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-1487.62,  -378.60, 39.20)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1135.7, -982.79, 45.46)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(373.55, 325.52, 102.60)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1163.67, -323.92, 68.26)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(2557.44, 382.03, 107.65)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-3039.16, 585.71, 6.95)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-3242.11, 1001.20, 11.86)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-2967.78, 391.49, 14.06)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-1820.38, 792.69, 137.16)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(547.75, 2671.53, 41.20)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1165.36, 2709.45, 37.20)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(2678.82, 3280.36, 54.27)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1961.17, 3740.5, 31.38)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1393.13, 3605.2, 33.99)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(1728.78, 6414.41, 34.06)},
+    {title = "Magasin Alimentaire", scale = 0.7, colour=43, id=628, coords = vector3(-2065.37, -329.56, 17.67)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(20.22, -1112.08, 42.37)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(-663.33, -946.43, 21.83)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(-331.86, 6084.98, 30.45)}, 
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(253.92, -50.43, 68.94)}, 
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(842.51, -1035.33, 27.19)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(810.05, -2159.21, 28.62)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(-1119.06, 2699.75, 17.55)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(2567.92, 292.51, 107.73)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(1692.04, 3760.88, 33.71)},
+    {title = "Ammu-nation", scale = 0.7, colour=1, id=313, coords = vector3(-1305.40, -394.12, 36.69)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id=73, coords = vector3(-1193.16, -767.98, 16.35)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-822.42, -1073.55, 10.35)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(75.34, -1393.00, 28.40)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-709.86, -153.1, 36.46)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-163.37, -302.73, 38.76)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(425.59, -806.15, 28.52)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-1450.42, -237.66, 48.85)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(4.87, 6512.46, 30.91)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(125.77, -223.9, 53.60)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(1693.92, 4822.82, 41.10)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(614.19, 2762.79, 41.12)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(1196.61, 2710.25, 37.26)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-3170.54, 1043.68, 19.90)},
+    -- {title = "Magasins de Vêtements", scale = 0.7, colour=81, id= 73, coords = vector3(-1101.48, 2710.57, 18.16)},
+    {title = "Hopital", scale = 0.7, colour = 1, id = 61, coords = vector3(-1856.66, -340.27, 49.43)},
+    {title = "Pharmacie", scale = 0.7, colour = 2, id = 51, coords = vector3(-1833.24, -379.02, 49.40)},
+}
+
+Citizen.CreateThread(function()
+    for key, value in pairs(blips) do
+        value.blip = AddBlipForCoord(value.coords)
+        SetBlipSprite(value.blip, value.id)
+        SetBlipDisplay(value.blip, 4)
+        SetBlipScale(value.blip, value.scale)
+        SetBlipColour(value.blip, value.colour)
+        SetBlipAsShortRange(value.blip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(value.title)
+        EndTextCommandSetBlipName(value.blip)
+    end
+end)
